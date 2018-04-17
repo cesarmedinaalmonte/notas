5aa8af3a31ce21ac8559edfcfe4b5c3464f6c153
Cesar Medina Almonte <cesarmedinaalmonte@hotmail.com>
2018-04-17 00:26:55 -0400

    codigo borrado de clase antiguas y clase datosbasedt borrada

--- notas/profesor.cs
+++ notas/profesor.cs
@@ -24,32 +24,9 @@ namespace notas
 
         private void btnguardarp_Click(object sender, EventArgs e)
         {
-            datosget datosget = new datosget();
 
 
-            datosget.nombre = txtnombrep.Text;
-            datosget.correo = txtemailp.Text;
-            datosget.telefono = txttelp.Text;
-           
-
-
-
-            int resultado = datosbasedt.agregar1(datosget);
-
-
-            if (resultado > 0)
-            {
-
-                MessageBox.Show("Datos de Seccion Guardados Correctamente", "Datos Guardados ", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-
-            }
-            else
-            {
-                MessageBox.Show(" No se Pudieron Guardar Datos de Seccion  ", "Error al Guardar ", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
         }
-
         private void btnatras_Click(object sender, EventArgs e)
         {
             Form formp = new inicio();
